ab27f8fbc21faf7a8c9c17aba52057b90e5427c3
keepcalm444 <keepcalm444@gmail.com>
2014-10-18 22:27:20 +1100

    Cardview-ise NoticesFragment. Looks pretty bad.

--- app/src/main/java/com/sbhstimetable/sbhs_timetable_android/NoticesFragment.java
+++ app/src/main/java/com/sbhstimetable/sbhs_timetable_android/NoticesFragment.java
@@ -40,6 +40,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;

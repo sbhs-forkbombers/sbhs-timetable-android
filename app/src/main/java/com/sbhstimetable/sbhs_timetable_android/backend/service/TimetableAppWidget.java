94ad12e3ab1985f440c74eea632dc7150d0aaca2
keepcalm444 <keepcalm444@gmail.com>
2014-10-21 22:09:36 +1100

    Also make the countdown widget respect transparency

--- app/src/main/java/com/sbhstimetable/sbhs_timetable_android/backend/service/TimetableAppWidget.java
+++ app/src/main/java/com/sbhstimetable/sbhs_timetable_android/backend/service/TimetableAppWidget.java
@@ -25,15 +25,21 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.appwidget.AppWidgetProviderInfo;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.widget.RemoteViews;
 
 import com.sbhstimetable.sbhs_timetable_android.R;
 import com.sbhstimetable.sbhs_timetable_android.backend.ApiAccessor;
 import com.sbhstimetable.sbhs_timetable_android.backend.DateTimeHelper;
+import com.sbhstimetable.sbhs_timetable_android.backend.internal.PrefUtil;
 import com.sbhstimetable.sbhs_timetable_android.backend.json.BelltimesJson;
 
 
@@ -44,11 +50,32 @@ public class TimetableAppWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.timetable_app_widget);
+        int home[] = new int[appWidgetIds.length];
+        int lock[] = new int[appWidgetIds.length];
+        int lockIdx = 0;
+        int homeIdx = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) { // no lockscreen widgets < 4.2, so don't check.
+            for (int i : appWidgetIds) {
+                if (appWidgetManager.getAppWidgetOptions(i).getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, -1) == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD) {
+                    lock[lockIdx++] = i;
+                } else {
+                    home[homeIdx++] = i;
+                }
+            }
+        }
+        else {
+            home = appWidgetIds;
+            homeIdx = appWidgetIds.length;
+        }
+
+
         BelltimesJson b = BelltimesJson.getInstance();
+        String label = "";
+        String in = "";
+        String cntDwn = "";
         if (b == null) {
             ApiAccessor.getBelltimes(context);
-            views.setTextViewText(R.id.widget_next_period, "Loading…");
+            cntDwn = "Loading…";
         }
         else {
             if (DateTimeHelper.bells == null) {
@@ -56,17 +83,43 @@ public class TimetableAppWidget extends AppWidgetProvider {
             }
             long time = DateTimeHelper.milliSecondsUntilNextEvent();
             BelltimesJson.Bell bell = b.getNextBell();
-            String label = "School starts";
-            String in = "in";
+            label = "School starts";
+            in = "in";
             if (bell != null) {
                 label = bell.getLabel();
                 in = "ends in";
             }
+            cntDwn = DateTimeHelper.formatToCountdown(time);
+
+        }
+        SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(context);
+        if (homeIdx > 0) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.timetable_app_widget);
+            String c = "#";
+            String trans = p.getString(PrefUtil.WIDGET_TRANSPARENCY_HS, "32");
+            c += "00".substring(trans.length()) + trans;
+            c += "000000";
+            views.setInt(R.id.widget_countdown_root, "setBackgroundColor", Color.parseColor(c));
             views.setTextViewText(R.id.widget_label, label);
             views.setTextViewText(R.id.widget_in, in);
-            views.setTextViewText(R.id.widget_next_period, DateTimeHelper.formatToCountdown(time));
+            views.setTextViewText(R.id.widget_next_period, cntDwn);
+            appWidgetManager.updateAppWidget(home, views);
         }
-        appWidgetManager.updateAppWidget(appWidgetIds, views);
+
+        if (lockIdx > 0) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.timetable_app_widget);
+            String c = "#";
+            String trans = p.getString(PrefUtil.WIDGET_TRANSPARENCY_LS, "32");
+            c += "00".substring(trans.length()) + trans;
+            c += "000000";
+            views.setInt(R.id.widget_countdown_root, "setBackgroundColor", Color.parseColor(c));
+            views.setTextViewText(R.id.widget_label, label);
+            views.setTextViewText(R.id.widget_in, in);
+            views.setTextViewText(R.id.widget_next_period, cntDwn);
+            appWidgetManager.updateAppWidget(lock, views);
+        }
+
+
         AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent m = new Intent(context, this.getClass());
         m.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);

eadaa6d6e16061a6bd325f7c91619c2bb2f17ee9
keepcalm444 <keepcalm444@gmail.com>
2015-01-28 23:20:39 +1100

    Progress on #17: the NotificationService works!
    Don't start it. It says "Hi there!" every five seconds.
    Seriously quite annoying. 0/10. But great debugging - 10/10

--- app/src/main/java/com/sbhstimetable/sbhs_timetable_android/backend/service/NotificationService.java
+++ app/src/main/java/com/sbhstimetable/sbhs_timetable_android/backend/service/NotificationService.java
@@ -1,16 +1,20 @@
 package com.sbhstimetable.sbhs_timetable_android.backend.service;
 
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
 
 public class NotificationService extends Service {
 	private static final String TAG = "NotificationService";
-
 	@Override
 	public IBinder onBind(Intent intent) {
 		// nope
@@ -20,13 +24,20 @@ public class NotificationService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.i(TAG, "Received start id " + startId + ": " + intent);
+		Toast.makeText(this.getApplicationContext(), "Hi there!", Toast.LENGTH_SHORT).show();
+		Intent me = new Intent(this, this.getClass());
+		PendingIntent soon = PendingIntent.getService(this, 0, me, 0);
+		AlarmManager am = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
 
+		am.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 5000, soon);
+		Log.i(TAG, "Set alarm for 5 seconds");
 		return START_STICKY;
 	}
 
 	@Override
 	public void onCreate() {
-		Toast.makeText(this.getApplicationContext(), "Hi there!", Toast.LENGTH_SHORT).show();
+
 		Log.w(TAG, "Created!");
+
 	}
 }
